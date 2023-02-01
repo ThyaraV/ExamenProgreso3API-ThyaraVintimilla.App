357a8732c0d253e51087368101bd562325ed00a1
Thyara <thyara.vintimilla@udla.edu.ec>
2023-02-01 09:33:06 -0600

    Arreglos CRUD

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,12 +2,14 @@
 using System.Net;
 using SQLite;
 using Newtonsoft.Json;
+using ExamenProgreso3API_ThyaraVintimilla.TV_Data;
+
 
 namespace ExamenProgreso3API_ThyaraVintimilla;
 
 public partial class MainPage : ContentPage
 {
-    private SQLiteConnection _connection;
+ //   private SQLiteConnection _connection;
 
     public MainPage()
     {
@@ -33,9 +35,31 @@ public partial class MainPage : ContentPage
             var resultado = JsonConvert.DeserializeObject<List<TV_Root>>(content);
             ListaDemo.ItemsSource = resultado;
         }
-        //var busqueda = new TV_Root { word = entry.Text };
-        // _connection.Insert(busqueda);
+
     }
 
+    private void TV_CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        TV_Root burgerTV = e.CurrentSelection.FirstOrDefault() as TV_Root;
+        if (burgerTV == null)
+            return;
+        Shell.Current.GoToAsync(nameof(MainPage), true, new Dictionary<string, object>
+        {
+            {"Item",burgerTV }
+        });
+        ((CollectionView)sender).SelectedItem = null;
+    }
+
+
+
+    /* string word = Buscador.Text;
+     string url = "https://dictionaryapi.dev/api/v1/entries/en/" + word;
+     var respuesta = await client.GetStringAsync(url);
+     Rootobject root = JsonConvert.DeserializeObject<Rootobject>(respuesta);
+     //var busqueda = new TV_Root { word = entry.Text };
+     // _connection.Insert(busqueda);
+     TV_APIDataBase db = new TV_APIDataBase(DependencyService.Get<IFileHelper>().GetLocalFilePath("tv_api.db3"));
+     db.GetRoot(root.Property1[0]);*/
 }
+
 

5825be92b58fb5ad8cb3861bf42a537993dc15b5
Thyara <thyara.vintimilla@udla.edu.ec>
2023-02-01 08:34:08 -0600

    Creación del ViewModel

---
+++ TV_ViewModels/TV_DateViewModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenProgreso3API_ThyaraVintimilla.TV_ViewModels
+{
+    public class TV_DateViewModel : INotifyPropertyChanged
+    {
+        DateTime _dateTime;
+        Timer _timer;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DateTime DateTime
+        {
+            get => _dateTime; set
+            {
+                if (_dateTime != value)
+                {
+                    _dateTime = value;
+                    OnPropertyChanged();
+
+                }
+            }
+        }
+
+        public TV_DateViewModel()
+        {
+            this.DateTime = DateTime.Now;
+            _timer = new Timer(new TimerCallback((s) => this.DateTime = DateTime.Now), null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+        }
+
+        ~TV_DateViewModel() => _timer.Dispose();
+
+        public void OnPropertyChanged([CallerMemberName] string name = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+
+
+
+    }
+}

a8f516e6d8d0a80c598d64fd823ccf87906482e5
Thyara <thyara.vintimilla@udla.edu.ec>
2023-02-01 08:27:30 -0600

    Método para LLAMr api

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,41 @@
-﻿namespace ExamenProgreso3API_ThyaraVintimilla;
+﻿using ExamenProgreso3API_ThyaraVintimilla.TV_Models;
+using System.Net;
+using SQLite;
+using Newtonsoft.Json;
+
+namespace ExamenProgreso3API_ThyaraVintimilla;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    private SQLiteConnection _connection;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+        //_connection = DependencyService.Get<ISQLite>().GetConnection();
+        //_connection.CreateTable<TV_Root>();
+    }
+    public async void Button_Clicked(object sender, EventArgs e)
+    {
+        string cadena = Buscador.Text;
+        var request = new HttpRequestMessage();
+        //request.RequestUri = new Uri("https://documenter.getpostman.com/view/10808728/SzS8rjbc#00030720-fae3-4c72-8aea-ad01ba17adf8");
+        request.RequestUri = new Uri("https://api.dictionaryapi.dev/api/v2/entries/en/" + cadena);
+        request.Method = HttpMethod.Get;
+        request.Headers.Add("Accept", "application/json");
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+        var client = new HttpClient();
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        HttpResponseMessage response = await client.SendAsync(request);
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            String content = await response.Content.ReadAsStringAsync();
+            var resultado = JsonConvert.DeserializeObject<List<TV_Root>>(content);
+            ListaDemo.ItemsSource = resultado;
+        }
+        //var busqueda = new TV_Root { word = entry.Text };
+        // _connection.Insert(busqueda);
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
 
